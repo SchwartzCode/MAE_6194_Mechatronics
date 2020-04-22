c7b96c7f0b995100066292c92b380bea5e940b50
SchwartzCode <jonbs98@gmail.com>
2020-04-22 13:44:35 -0400

    Proving quaternion multiplication equations are equivalent (sorta... tbh I'd rather just prove this by hand than running a bunch of inputs and looking at outputs

---
+++ hw6_quaternions.py
@@ -0,0 +1,12 @@
+from sympy import *
+init_printing(use_unicode=True)
+import numpy as np
+
+theta1 = np.pi/6
+theta2 = np.pi/3
+a1 = np.array([1,0,0]).reshape(3,1)
+a2 = np.array([0,0,1]).reshape(3,1)
+
+Matrix([cos(theta1 / 2), 1, 2])
+
+#sin(theta1 / 2) *a1
